10b98d1dbeecc148f2222d8a03170978e35980eb
Markus Wallerberger <mwallerb@umich.edu>
2019-08-22 15:48:30 +0200

    Add copyright notice.

--- tests/test_sources.py
+++ tests/test_sources.py
@@ -1,3 +1,10 @@
+"""
+Tests for Fortran files in data/
+
+Copyright 2019 Markus Wallerberger.
+Released under the GNU Lesser General Public License, Version 3 only.
+See LICENSE.txt for permissions on usage, modification and distribution
+"""
 from __future__ import print_function
 import sys
 import os.path
