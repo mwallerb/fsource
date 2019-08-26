ee976338f2de6417f092070b531809641a2b3fb0
Markus Wallerberger <mwallerb@umich.edu>
2019-08-26 15:27:50 +0200

    Add syntax writer.

--- src/fsource/analyzer.py
+++ src/fsource/analyzer.py
@@ -6,6 +6,8 @@ See LICENSE.txt for permissions on usage, modification and distribution
 from __future__ import print_function
 from collections import OrderedDict
 
+import io
+
 from . import parser
 from . import lexer
 from . import common
@@ -29,31 +31,99 @@ def sexpr_transformer(branch_map, fallback):
     return transformer
 
 
-class Ignored:
+class SyntaxWriter:
+    @classmethod
+    def getstr(cls, obj):
+        out = cls()
+        obj.write_code(out)
+        return str(out)
+
+    def __init__(self, out=None):
+        if out is None:
+            out = io.StringIO()
+        self.out = out
+        self.indenttext = ""
+        self.level = 0
+
+    def write(self, text):
+        self.out.write(text)
+
+    def writeline(self, line):
+        self.out.write(self.indenttext + line + "\n")
+
+    def indent(self, line=None):
+        if line is not None:
+            self.writeline(line)
+        self.level += 1
+        self.indenttext = "    " * self.level
+
+    def unindent(self, line=None):
+        if self.level == 0:
+            raise ValueError("Unable to unindent")
+        self.level -= 1
+        self.indenttext = "    " * self.level
+        if line is not None:
+            self.writeline(line)
+
+    def __str__(self):
+        return self.out.getvalue()
+
+
+class Node(object):
+    def __str__(self):
+        return SyntaxWriter.getstr(self)
+
+    def write_code(self):
+        raise NotImplementedError("Not implemented")
+
+
+class Ignored(Node):
     """Ignored node of the AST"""
     def __init__(self, *ast):
+        super().__init__()
         self.ast = ast
 
-    def __str__(self): return self.get_code()
-
-    def get_code(self, ls="\n", endls="\n"):
-        return " &%s!IGNORED: %s%s" % (ls, repr(self.ast), ls)
+    def write_code(self, out):
+        out.writeline("(IGNORED: %s)"  % repr(self.ast))
 
 
-class CompilationUnit:
+class CompilationUnit(Node):
     """Top node representing one file"""
     def __init__(self, ast_version, fname, *objs):
+        super().__init__()
         self.ast_version = ast_version
         self.filename = fname
         self.objs = objs
 
-    def get_code(self, ls="\n", endls="\n"):
-        s = "! FILE %s%s" % (self.filename, ls)
-        s += "! AST VERSION %s%s" % (self.ast_version, ls)
+    def write_code(self, out):
+        out.writeline("! FILE %s" % self.filename)
+        out.writeline("! AST VERSION %s" % (self.ast_version,))
         for obj in self.objs:
-            s += obj.get_code(ls)
-        s += "! END FILE %s%s" % (self.filename, endls)
-        return s
+            obj.write_code(out)
+            out.writeline("")
+        out.writeline("! END FILE %s" % self.filename)
+
+
+class Module(Node):
+    def __init__(self, name, decls, contained):
+        super().__init__()
+        self.name = name
+        self.decls = decls
+        self.contained = contained
+
+    def imbue(self, parent):
+        parent.modules[self.name] = self
+        self.handle_children()
+
+    def write_code(self, out):
+        out.indent("MODULE %s" % self.name)
+        for decl in self.decls:
+            decl.write_code(out)
+        out.unindent()
+        out.indent("CONTAINS")
+        for obj in self.contained:
+            obj.write_code(out)
+        out.unindent("END MODULE %s" % self.name)
 
 
 def unpack(arg):
@@ -71,16 +141,22 @@ def unpack_sequence(fn=None):
 
 
 HANDLERS = {
-    'compilation_unit': CompilationUnit,
-    'filename':         unpack,
-    'ast_version':      unpack_sequence(),
+    'compilation_unit':  CompilationUnit,
+    'filename':          unpack,
+    'ast_version':       unpack_sequence(),
+
+    'module_decl':       Module,
+    'declaration_block': unpack_sequence(),
+    'contained_block':   unpack_sequence(),
+
+    'id':                lambda name: name.lower(),
     }
 
 TRANSFORMER = sexpr_transformer(HANDLERS, Ignored)
 
 if __name__ == '__main__':
-    fname = '../tests/data/lexfixed.f'
+    fname = '../tests/data/simple.f90'
     slexer = lexer.lex_buffer(open(fname))
     ast = parser.compilation_unit(parser.TokenStream(slexer, fname=fname))
     asr = TRANSFORMER(ast)
-    print (asr.get_code())
+    print (str(asr), end="")
