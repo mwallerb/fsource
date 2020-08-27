8f52682736b01debd3eb2131a648fe218306cc49
Markus Wallerberger <mwallerb@umich.edu>
2020-08-27 11:52:04 +0200

    Fix linting bug.

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -18,5 +18,8 @@ def test_linting_errors():
     herepath = os.path.dirname(os.path.realpath(__file__))
     srcdir = os.path.join(herepath, os.pardir, "src", "fsource")
     print("running `pylint -E {}`".format(srcdir))
-    run = pylint_lint.Run(['-E', srcdir], exit=False)
+
+    # do_exit is deprecated, however, exit= is only available from and
+    # version 2.5.1 onwards.  Nice job of semantic versioning, guys!
+    run = pylint_lint.Run(['-E', srcdir], do_exit=False)
     assert run.linter.msg_status == 0
